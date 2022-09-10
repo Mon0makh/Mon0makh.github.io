d3da1a5d8c4bcc2848db586a77d538d8a08a1b60
Vladimir Monomakh <red.freedomight@gmail.com>
2022-09-10 23:09:02 +0600

    update all site

--- assets/js/init.js
+++ assets/js/init.js
@@ -71,7 +71,7 @@ jQuery( document ).ready(function( $ ) {
 	$(window).scroll(function() {
 		var height = $(window).height();
 		var scroll = $(window).scrollTop();
-		if (scroll) {
+		if (scroll > 100) {
 			$(".header-hide").addClass("scroll-header");
 		} else {
 			 $(".header-hide").removeClass("scroll-header");
